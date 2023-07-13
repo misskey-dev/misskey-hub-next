a1f0178e4d947c45e6fe6dffefc2130ea4a77d5a
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-07-14 01:00:20 +0900

    (fix) failure on prerender

--- components/content/MkIndex.vue
+++ components/content/MkIndex.vue
@@ -45,7 +45,7 @@ const findDeepObject = (obj: Record<string, any>, condition: (v: any) => boolean
 		return obj;
 	}
 
-	if (obj.children && obj.children.length > 0) {
+	if (obj?.children && obj.children.length > 0) {
 		for (let i = 0; i < obj.children.length; i++) {
             console.log(obj.children[i]);
 			const result = findDeepObject(obj.children[i], condition);
