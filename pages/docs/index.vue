4761486d6afe59c6dd776f6d464d87dca258591b
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-07-15 17:33:31 +0900

    (refactor) separate LargeLinks component

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -9,7 +9,7 @@
                 <div class="hidden lg:block relative px-6 py-8">
                     <GDots class="absolute top-0 left-0 w-32 h-32 text-accent-600" />
                     <GDots class="absolute bottom-0 right-0 w-32 h-32 text-accent-600" />
-                    <div class="relative lg:w-72 rotate-6">
+                    <div class="relative lg:w-64 rotate-6">
                         <img class="drop-shadow-xl" src="/img/emojis/green_book_3d.png" />
                     </div>
                 </div>
@@ -31,28 +31,21 @@
                         anchor: '#forDevelopers',
                     }
                 ]" />
-                <GNuxtLink :to="localePath(`/docs/about-misskey`)" class="rounded-xl border border-slate-200 dark:border-accent-900 transition-colors hover:bg-slate-100 dark:hover:bg-slate-800 p-4 sm:pt-0 sm:pb-0 sm:pl-6 sm:pr-0 overflow-hidden flex">
-                    <div class="flex flex-col justify-center">
-                        <h2 class="text-xl sm:text-2xl font-bold mb-2">{{ $t('_docs._aboutMisskey.title') }}<ArrowRightIco class="ml-2" /></h2>
-                        <p class="text-slate-500 dark:text-slate-300">{{ $t('_docs._aboutMisskey.description') }}</p>
-                    </div>
-                    <div class="hidden sm:block ml-auto flex-shrink-0 relative pt-4 pr-4 h-48 w-auto">
-                        <GDots class="absolute top-0 right-0 h-20 w-20 text-accent-600" />
-                        <GDots class="absolute bottom-0 -left-2 h-14 w-20 text-accent-600" />
-                        <img class="relative h-full" src="/img/docs/mi_ai_docs.png" />
-                    </div>
-                </GNuxtLink>
-                <GNuxtLink :to="localePath(`/docs/releases`)" class="rounded-xl border border-slate-200 dark:border-accent-900 transition-colors hover:bg-slate-100 dark:hover:bg-slate-800 p-4 sm:pt-0 sm:pb-0 sm:pl-6 sm:pr-0 overflow-hidden flex">
-                    <div class="flex flex-col justify-center">
-                        <h2 class="text-xl sm:text-2xl font-bold mb-2">{{ $t('_docs._changelog.title') }}<ArrowRightIco class="ml-2" /></h2>
-                        <p class="text-slate-500 dark:text-slate-300">{{ $t('_docs._changelog.description') }}</p>
-                    </div>
-                    <div class="hidden sm:block ml-auto flex-shrink-0 relative py-4 pr-4 h-48 w-auto">
-                        <GDots class="absolute top-0 right-0 h-20 w-20 text-accent-600" />
-                        <GDots class="absolute bottom-0 -left-2 h-14 w-20 text-accent-600" />
-                        <img class="relative h-full" src="/img/emojis/rocket_3d.png" />
-                    </div>
-                </GNuxtLink>
+                <GLargeLinks :items="[
+                    {
+                        to: localePath('/docs/about-misskey/'),
+                        title: $t('_docs._aboutMisskey.title'),
+                        description: $t('_docs._aboutMisskey.description'),
+                        img: '/img/docs/mi_ai_docs.png',
+                        cutBottom: true,
+                    },
+                    {
+                        to: localePath('/docs/releases'),
+                        title: $t('_docs._changelog.title'),
+                        description: $t('_docs._changelog.description'),
+                        img: '/img/emojis/rocket_3d.png',
+                    }
+                ]" />
                 <DocsReadersNav section-id="forUsers" id="forUsers" />
                 <!--<DocsReadersNav section-id="forAdmin" id="forAdmin" />
                 <DocsReadersNav section-id="forDevelopers" id="forDevelopers" />-->
@@ -63,6 +56,7 @@
 
 <script setup lang="ts">
 import ArrowRightIco from 'bi/arrow-right.svg';
+import { title } from 'process';
 
 const localePath = useLocalePath();
 </script>
