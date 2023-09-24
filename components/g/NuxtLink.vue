454d06f140518f12f8cca9c82874bef4872f86bd
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-09-24 12:58:17 +0900

    (Fix) GNuxtLink

--- components/g/NuxtLink.vue
+++ components/g/NuxtLink.vue
@@ -11,6 +11,12 @@
 import { parseURL, cleanDoubleSlashes, withTrailingSlash } from 'ufo';
 import { RouteLocationRaw } from '#vue-router';
 
+/**
+ * TrailingSlashをつけている（pnpm generate時の出力ディレクトリ構造の関係）ので、
+ * 二重にスラッシュが入って無限ループに陥らないようにするための
+ * NuxtLinkのラッパーコンポーネント
+ */
+
 const rawProps = defineProps<{
     to?: RouteLocationRaw | string;
     href?: RouteLocationRaw | string;
@@ -25,11 +31,13 @@ const realHref = computed(() => {
         const url = parseURL(rhf);
 
         if (!url.host || rootDomain.host === url.host) {
-            return withTrailingSlash(rhf, true);
+            return withTrailingSlash(cleanDoubleSlashes(rhf), true);
         }
 
-        return cleanDoubleSlashes(rhf);
+        return rhf;
     }
+
+    // TODO: ルート定義をオブジェクトで渡された時のバリデーション
 
     return rhf;
 });
