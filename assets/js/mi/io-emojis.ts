584ba5f5389e01b582580695d0c818550deddefa
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-09-26 21:24:01 +0900

    (Fix) build error

--- assets/js/mi/io-emojis.ts
+++ assets/js/mi/io-emojis.ts
@@ -2,8 +2,12 @@ import { getIOEmoji } from './io-meta';
 import * as Misskey from 'misskey-js';
 export const customEmojisMap = new Map<string, Misskey.entities.CustomEmoji>();
 
-const emojis = await getIOEmoji();
+async function init() {
+    const emojis = await getIOEmoji();
 
-for (const emoji of emojis.emojis) {
-    customEmojisMap.set(emoji.name, emoji);
+    for (const emoji of emojis.emojis) {
+        customEmojisMap.set(emoji.name, emoji);
+    }
 }
+
+init();
