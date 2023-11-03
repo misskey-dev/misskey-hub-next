c555a762c98b9036b0ca636a22749604e5fbe017
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-11-03 16:55:33 +0900

    (enhance) fonts in mainland china

--- app.vue
+++ app.vue
@@ -76,8 +76,14 @@ const head = useLocaleHead({
  * fonts.googleapis.com → fonts.googleapis.cn
  **/
 const cnHead = (locale.value === 'cn') ? [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.cn' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.cn' },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.cn/css2?family=Capriola&family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
-] : [];
+] : [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Capriola&family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap' },
+];
 
 useHead((): Record<string, any> => ({
     htmlAttrs: {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,9 +40,6 @@ export default defineNuxtConfig({
 	app: {
 		head: {
 			link: [
-				{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-				{ rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-				{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Capriola&family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap' },
 				{ rel: 'stylesheet', href: '/fonts/fonts.css' },
 			],
 		},
