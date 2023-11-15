ff43ac695b09f43ac60596472953d5fd788c4ca8
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-11-15 21:23:19 +0900

    (fix) noscript message

--- app.vue
+++ app.vue
@@ -126,10 +126,7 @@ useHead((): Record<string, any> => ({
 </script>
 <template>
     <div class="text-slate-800 dark:text-slate-200 bg-slate-100 dark:bg-gray-900">
-        <NuxtIsland name="commonNoScript">
-            <noscript class="block bg-accent-800 text-white text-center py-1.5 px-3 keep-all relative z-[10005]">Please turn
-                on Javascript from your browser's settings.</noscript>
-        </NuxtIsland>
+        <NuxtIsland name="GNoScript" />
         <NuxtLayout>
             <NuxtPage />
         </NuxtLayout>
---
+++ components/g/NoScript.server.vue
@@ -0,0 +1,11 @@
+<template>
+    <noscript class="block bg-accent-800 text-white text-center py-1.5 px-3 keep-all relative z-[10005]">Please turn on Javascript from your browser's settings.</noscript>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
