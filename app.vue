bbd831fa6b3a6cb0c92f4b7d205755598d88a894
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-12-01 01:38:00 +0900

    fix path

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
 import type { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables';
 import NProgress from 'nprogress';
 import type { Graph, Thing } from 'schema-dts';
+import { normalizeURL, withTrailingSlash } from 'ufo';
 
 const nuxtApp = useNuxtApp();
 
@@ -69,7 +70,7 @@ const getLdJson = (additionalGraphes: Thing[] = []): string => {
 const currentLocaleIso = computed(() => (locales.value as LocaleObject[]).find((e) => e?.code === locale.value)?.iso);
 
 const head = useLocaleHead({
-    addSeoAttributes: true
+    addSeoAttributes: true,
 });
 
 /** 
@@ -117,7 +118,7 @@ useHead((): Record<string, any> => ({
         ...(head.value.meta?.map((e) => ({ property: e.property, content: e.content, })) || []),
     ],
     link: [
-        ...(head.value.link?.map((e) => ({ rel: e.rel, href: (e.href.endsWith('/') ? e.href : e.href + '/'), hreflang: e.hreflang, })) || []),
+        ...(head.value.link?.map((e) => ({ rel: e.rel, href: normalizeURL(withTrailingSlash(e.href)), hreflang: e.hreflang, })) || []),
         ...cnHead,
     ],
     script: [
