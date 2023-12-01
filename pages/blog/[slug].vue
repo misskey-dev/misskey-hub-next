238cd716fa8881d4fb50beb148c7cb16ef109506
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-12-01 23:55:20 +0900

    (fix) ブログページのリンクから元の言語に戻れるように

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="mx-auto container max-w-screen-md relative px-6 py-5">
-            <GNuxtLink :to="localePath('/blog/')" class="absolute top-2.5 left-6 p-2.5 opacity-40">
+            <GNuxtLink :to="localePath('/blog/', originalLocale)" class="absolute top-2.5 left-6 p-2.5 opacity-40">
                 <LeftIco class="mr-2 stroke-1 stroke-current" />
             </GNuxtLink>
             <p class="text-center mb-4">{{ $t('_blog.title') }}</p>
@@ -13,7 +13,7 @@
                 <ContentRenderer :value="data" />
             </div>
             <div class="text-center mt-6 lg:mt-12">
-                <GNuxtLink :to="localePath('/blog/')" class="text-xl font-bold hover:opacity-70">
+                <GNuxtLink :to="localePath('/blog/', originalLocale)" class="text-xl font-bold hover:opacity-70">
                     <LeftIco class="mr-2" />{{ $t('_blog.back') }}
                 </GNuxtLink>
             </div>
@@ -29,6 +29,9 @@ import type { MiBlogParsedContent } from '~/types/content';
 defineI18nRoute({
     locales: ['ja'],
 });
+
+// 他言語からやってきたときに正しいパスに戻す
+const originalLocale = useState('miHub_blog_originalLocale', () => 'ja');
 
 const localePath = useLocalePath();
 
