6c8bcb2599ed0994e926d249f3f227fd607f244a
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-12-03 18:58:58 +0900

    fix layout

--- pages/docs/[...slug].vue
+++ pages/docs/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid docs-main">
+    <div class="grid" :class="isEPDocs ? 'ep-docs-main' : 'docs-main'">
         <div class="lg:hidden sticky top-16 -mx-6 -mt-6 overflow-y-auto bg-slate-50 dark:bg-slate-900 z-[9890] border-b dark:border-slate-700 text-sm flex items-start">
             <details v-if="data?.body && data.body.toc?.links.length > 0" class="peer order-2 flex-grow flex-shrink-0" :open="openState">
                 <summary class="py-4 cursor-pointer">
@@ -38,7 +38,7 @@
                 </div>
             </template>
         </div>
-        <div class="hidden lg:block text-sm">
+        <div v-if="!isEPDocs" class="hidden lg:block text-sm">
             <div class="sticky top-16 h-[calc(100vh-4rem)] overflow-y-auto py-6 pl-6">
                 <h3 class="font-bold mb-6">{{ $t('_docs._toc.title') }}</h3>
                 <DocsTocLinks v-if="data?.body" :links="data?.body.toc?.links" :max-depth="data?.maxTocDepth ?? undefined" class="break-words" />
@@ -62,16 +62,22 @@ definePageMeta({
 });
 
 const route = useRoute();
-const slugs = (route.params.slug as string[]).filter((v) => v !== '');
+const slugs = (Array.isArray(route.params.slug) ? route.params.slug : [route.params.slug]).filter((v) => v !== '');
+const isEPDocs = ref((slugs[0] === 'for-developers' && slugs[1] === 'api' && slugs[2] === 'endpoints' && slugs[3] != null));
+
+watch(() => route.path, (_) => {
+    const slugs = (Array.isArray(route.params.slug) ? route.params.slug : [route.params.slug]).filter((v) => v !== '');
+    isEPDocs.value = (slugs[0] === 'for-developers' && slugs[1] === 'api' && slugs[2] === 'endpoints' && slugs[3] != null);
+});
 
 let path = `/${locale.value}/docs/${slugs.join('/')}`;
 
-if (slugs[0] === 'for-developers' && slugs[1] === 'api' && slugs[2] === 'endpoints' && slugs[3]) {
+if (isEPDocs.value) {
     // APIドキュメントの翻訳はymlで当てるのでjsonは共通
     path = `/api-docs/${slugs.slice(2).join('/')}`;
 }
 
-const { data } = await useAsyncData(`blog-${locale.value}-${slugs.join('-')}`, () => queryContent<MiDocsParsedContent>(path).findOne());
+const { data } = await useAsyncData(`docs-${locale.value}-${slugs.join('-')}`, () => queryContent<MiDocsParsedContent>(path).findOne());
 
 if (!data.value) {
     throw createError({ statusCode: 404, statusMessage: 'page not found' });
@@ -84,6 +90,7 @@ if (data.value.description) {
 </script>
 
 <style scoped>
+.ep-docs-main,
 .docs-main {
     grid-template-columns: 1fr;
 }
