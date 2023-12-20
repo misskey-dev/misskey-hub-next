2b8fb86f973c4e92a809ee5cf74d9c1f40af6eff
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-12-20 19:37:59 +0900

    fix style

--- pages/about-us/team.vue
+++ pages/about-us/team.vue
@@ -24,8 +24,10 @@
                             <p>{{ $t('_aboutUs._team._core.description') }}</p>
                         </div>
                     </div>
-                    <div class="grid md:grid-cols-2 gap-4">
-                        <AboutUsTeamMember v-for="member in coreTeamMember" :member="member" />
+                    <div>
+                        <div class="grid md:grid-cols-2 gap-4">
+                            <AboutUsTeamMember v-for="member in coreTeamMember" :member="member" />
+                        </div>
                     </div>
                 </div>
                 <div class="grid gap-x-12 gap-y-4 w-full" :class="$style.teamRoot">
@@ -35,8 +37,10 @@
                             <p>{{ $t('_aboutUs._team._coreEmeriti.description') }}</p>
                         </div>
                     </div>
-                    <div class="grid md:grid-cols-2 gap-4">
-                        <AboutUsTeamMember v-for="member in coreTeamEmeriti" :member="member" />
+                    <div>
+                        <div class="grid md:grid-cols-2 gap-4">
+                            <AboutUsTeamMember v-for="member in coreTeamEmeriti" :member="member" />
+                        </div>
                     </div>
                 </div>
                 <div class="grid gap-x-12 gap-y-4 w-full" :class="$style.teamRoot">
@@ -46,15 +50,16 @@
                             <p>{{ $t('_aboutUs._team._contributors.description') }}</p>
                         </div>
                     </div>
-                    <div class="grid md:grid-cols-2 gap-4">
-                        <AboutUsTeamMember v-for="member in contributors" :member="member" />
-                        <GNuxtLink to="https://github.com/misskey-dev/misskey/graphs/contributors" target="_blank" class="block p-4 bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 rounded-lg">
-                            <div class="flex h-full w-full items-center">
-                                <div class="text-lg">{{ $t('_aboutUs._team._contributors.seeMore') }}</div>
-                                <ExtIco class="block ml-auto" />
-                            </div>
-                        </GNuxtLink>
-
+                    <div>
+                        <div class="grid md:grid-cols-2 gap-4">
+                            <AboutUsTeamMember v-for="member in contributors" :member="member" />
+                            <GNuxtLink to="https://github.com/misskey-dev/misskey/graphs/contributors" target="_blank" class="block p-4 bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 rounded-lg">
+                                <div class="flex h-full w-full items-center">
+                                    <div class="text-lg">{{ $t('_aboutUs._team._contributors.seeMore') }}</div>
+                                    <ExtIco class="block ml-auto" />
+                                </div>
+                            </GNuxtLink>
+                        </div>
                     </div>
                 </div>
             </div>
