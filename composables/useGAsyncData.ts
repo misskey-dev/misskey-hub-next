9c1dfb304ff4d8f50e4c8b05897b55fa3a9cc07a
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-12-23 23:00:20 +0900

    (fix) types

--- composables/useGAsyncData.ts
+++ composables/useGAsyncData.ts
@@ -1,8 +1,10 @@
-export function useGAsyncData(...params: Parameters<typeof useAsyncData>) {
+type KeysOf<T> = Array<T extends T ? keyof T extends string ? keyof T : never : never>;
+
+export function useGAsyncData<ResT, DataE = Error, DataT = ResT, PickKeys extends KeysOf<DataT> = KeysOf<DataT>, DefaultT = null>(...params: Parameters<typeof useAsyncData<ResT, DataE, DataT, PickKeys, DefaultT>>): ReturnType<typeof useAsyncData<ResT, DataE, DataT, PickKeys, DefaultT>> {
     if (!params[2]) {
         params[2] = {};
     }
     params[2].getCachedData = (key) => useNuxtData(key).data.value ?? null;
     
-    return useAsyncData(...params);
+    return useAsyncData<ResT, DataE, DataT, PickKeys, DefaultT>(...params);
 }
