9e660e1961d5e06755aa6aaca2317d17ded78fae
tassoman <tassoman@gmail.com>
2024-03-09 17:34:09 +0100

    Italian call to action, italian about, Italian changelog

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ export default defineAppConfig({
         title: {
             ja: "v2024.3.1をリリースしました！",
             en: "v2024.3.1 is out now!",
+						it: "La versione 2024.3.1 è pronta!"
         },
         to: "/docs/releases/"
     },
