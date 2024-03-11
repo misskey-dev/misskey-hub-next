6b1ccefd9c25be53b0646a48dbbfc0ebadb9b9d3
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-03-11 18:26:08 +0900

    update deps

--- types/instances-info.ts
+++ types/instances-info.ts
@@ -1,4 +1,4 @@
-import type * as Misskey from 'misskey-js';
+import type { entities as MisskeyEntities } from 'misskey-js';
 
 /** 各インスタンスの情報 */
 export type InstanceItem = {
@@ -23,7 +23,7 @@ export type InstanceItem = {
     /** nodeinfo */
     nodeinfo: Record<string, any> | null,
     /** result of api/meta */
-    meta: Misskey.entities.InstanceMetadata | null,
+    meta: MisskeyEntities.MetaLite | null,
     /** Number of Notes per Day (15-day average) */
     npd15: number, 
     stats?: Record<string, any>,   //  deprecated (result of api/stats)
