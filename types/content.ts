0dd157a3069dfa946736289cc0d5e32919bc08ea
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-04-06 15:48:26 +0900

    feat(docs): ステップバイステップガイドを追加

--- types/content.ts
+++ types/content.ts
@@ -1,18 +1,41 @@
 // Misskey Docs Frontmatter Types
-import type { ParsedContent, MarkdownParsedContent } from '@nuxt/content/dist/runtime/types';
+import type { ParsedContent, MarkdownParsedContent, MarkdownRoot } from '@nuxt/content/dist/runtime/types';
 
 /**
  * Docs Frontmatter の型定義
  * 
  * `/content/<lang>/docs/` のフロントマターはこの形式で入力してください 
  */
-export interface MiDocsParsedContent extends MarkdownParsedContent {
+interface MiDocsParsedContentMd extends MarkdownParsedContent {
+    _TYPE_: undefined;
+
     /** もくじの見出しをさかのぼる限度 */
     maxTocDepth?: number;
 
     /** 前へ・次へボタンの階層考慮を無視 */
     ignoreDirBasedNav?: boolean;
-}
+};
+
+/**
+ * ステップバイステップガイドの型定義
+ */
+interface MiDocsParsedContentSteppedGuide extends ParsedContent {
+    _TYPE_: 'STEPPED_GUIDE';
+
+    guides: {
+        _AUTOSELECT_TYPE_?: 'OS_ANDROID' | 'OS_IOS';
+        _LAYOUT_TYPE_?: 'IMAGE_PORTRAIT_FIXED';
+        title: string;
+        description: string | MarkdownRoot;
+        steps: {
+            title: string;
+            description: string | MarkdownRoot;
+            image?: string;
+        }[];
+    }[];
+};
+
+export type MiDocsParsedContent = MiDocsParsedContentMd | MiDocsParsedContentSteppedGuide;
 
 /**
  * Blog Frontmatter の型定義
