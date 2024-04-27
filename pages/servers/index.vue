b9cdcc500e9ef97664e32f1e87341144b3297df3
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-04-27 11:12:00 +0900

    feat(ServerFinder): 免責事項を追加

--- pages/servers/index.vue
+++ pages/servers/index.vue
@@ -7,8 +7,14 @@
                 <I18nT scope="global" keypath="_servers.addYourServer" tag="span">
                     <GNuxtLink class="font-bold hover:underline underline-offset-4" to="https://github.com/joinmisskey/api">{{ $t('_servers.addYourServerLink') }}</GNuxtLink>
                 </I18nT>
-                <div class="!mt-2 text-sm p-3 rounded-lg bg-white dark:bg-slate-800 border border-gray-300 dark:border-gray-950">
-                    {{ $t('lastUpdate') }}: {{ updatedAt ? $d(updatedAt) : $t('loading') }}
+                <div class="!mt-2 space-y-2">
+                    <div class="text-sm p-3 rounded-lg bg-white dark:bg-slate-800 border border-gray-300 dark:border-gray-950">
+                        {{ $t('lastUpdate') }}: {{ updatedAt ? $d(updatedAt) : $t('loading') }}
+                    </div>
+                    <details class="group overflow-hidden text-sm rounded-lg bg-white dark:bg-slate-800 border border-gray-300 dark:border-gray-950">
+                        <summary class="p-3 cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-700 font-bold">{{ $t('_servers.disclaimerTitle') }}</summary>
+                        <div class="p-3 text-start border-t border-dashed border-gray-300 dark:border-gray-950">{{ $t('_servers.disclaimer') }}</div>
+                    </details>
                 </div>
             </template>
             <template #icon>
