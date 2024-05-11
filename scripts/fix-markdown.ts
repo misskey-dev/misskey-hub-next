b65cd7d866efb86fa831259f13d5b141a0ae6087
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-05-12 00:26:38 +0900

    use `@misskey-dev/markdown-space-autofixer`

---
+++ scripts/fix-markdown.ts
@@ -0,0 +1,32 @@
+import { promises as fsp } from 'fs';
+import { glob } from 'fast-glob';
+import { addSpaceBetweenEachSentence } from '@misskey-dev/markdown-space-autofixer';
+import { localesConst } from '../assets/data/locales';
+import type { LocaleCodes } from '../assets/data/locales';
+
+export async function fixMarkdown() {
+    const ignoreLocales: LocaleCodes[] = [
+        'ja',
+        'ja-ks',
+        'cn',
+        'ko',
+        'tw',
+    ];
+
+    // globs files
+    const filesToCheck = await glob(`../../content/(${localesConst.map((v) => v.code).filter((v) => !ignoreLocales.includes(v)).join('|')})/**/*.md`, {
+        ignore: [ '**/5.releases.md' ],
+    });
+
+    const jobs = filesToCheck.map(async (file) => {
+        const content = await fsp.readFile(file, 'utf-8');
+        const newContent = await addSpaceBetweenEachSentence(content);
+
+        if (content !== newContent) {
+            console.log(`Writing: ${file}`);
+            await fsp.writeFile(file, newContent);
+        }
+    });
+
+    await Promise.all(jobs);
+}
