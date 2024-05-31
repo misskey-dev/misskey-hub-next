876303f0065fda1ecbf8cc2f063bed4439d69d16
syuilo <4439005+syuilo@users.noreply.github.com>
2024-05-31 21:29:53 +0900

    2024.5.0

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
     notice: {
         title: {
-            ja: "v2024.3.1をリリースしました！",
-            en: "v2024.3.1 is out now!",
+            ja: "v2024.5.0をリリースしました！",
+            en: "v2024.5.0 is out now!",
         },
         to: "/docs/releases/"
     },
