02b2e2cbbfbcd48cb1bd8f68886d3535b51fdefb
syuilo <4439005+syuilo@users.noreply.github.com>
2024-07-23 11:53:48 +0900

    Update 8.mi-card.md

---
+++ components/content/X__Docs__mi-card__Calculator.vue
@@ -0,0 +1,32 @@
+<template>
+	<div :class="$style.calculator" class="mx-auto max-w-lg">
+		<label class="mb-1">合計の支援額</label>
+		<input v-model="paidAmount" :class="$style.input" type="number">
+		<div>合計受け取り可能枚数: <b>{{ cardAmount }}</b></div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const paidAmount = ref<number>(0);
+const cardAmount = computed(() => (paidAmount.value >= 20000 ? 1 : 0) + Math.floor(Math.max(0, paidAmount.value - 20000) / 30000));
+</script>
+
+<style module>
+.calculator {
+	@apply rounded-lg border border-slate-200 dark:border-slate-700;
+
+	display: block;
+}
+
+.input {
+    @apply border-slate-200;
+    flex-shrink: 1;
+    padding: 10px;
+    width: 100%;
+    height: 40px;
+    font-size: 16px;
+    border: solid 1px;
+    border-radius: 4px 0 0 4px;
+    -webkit-appearance: textfield;
+}
+</style>
