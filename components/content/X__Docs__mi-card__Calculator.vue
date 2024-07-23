13c67a8e48648608654bfe9e261f16a429a89b7e
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-07-23 14:13:05 +0900

    enhance(docs/micard): tweak calculator

--- components/content/X__Docs__mi-card__Calculator.vue
+++ components/content/X__Docs__mi-card__Calculator.vue
@@ -1,32 +1,22 @@
 <template>
-	<div :class="$style.calculator" class="mx-auto max-w-lg">
-		<label class="mb-1">合計の支援額</label>
-		<input v-model="paidAmount" :class="$style.input" type="number">
-		<div>合計受け取り可能枚数: <b>{{ cardAmount }}</b></div>
+	<div class="block mx-auto max-w-lg p-4 rounded-lg border border-slate-200 dark:border-slate-700">
+		<label for="miCardCalculatorAmount" class="inline-block form-label">合計の支援額</label>
+        <div class="input-group">
+            <input v-model="paidAmount" id="miCardCalculatorAmount" type="number" class="form-control !flex-grow">
+            <select v-model="currency" class="form-select !w-auto !flex-grow-0 !flex-shrink-0">
+                <option value="JPY">JPY</option>
+                <option value="USD">USD</option>
+            </select>
+        </div>
+        <div class="mt-4">合計受け取り可能枚数: <b>{{ cardAmount }}</b></div>
 	</div>
 </template>
 
 <script setup lang="ts">
 const paidAmount = ref<number>(0);
-const cardAmount = computed(() => (paidAmount.value >= 20000 ? 1 : 0) + Math.floor(Math.max(0, paidAmount.value - 20000) / 30000));
+const currency = ref<'USD' | 'JPY'>('JPY');
+const cardAmount = computed(() => {
+    const yen = paidAmount.value * (currency.value === 'USD' ? 150 : 1);
+    return (yen >= 20000 ? 1 : 0) + Math.floor(Math.max(0, yen - 20000) / 30000);
+});
 </script>
-
-<style module>
-.calculator {
-	@apply rounded-lg border border-slate-200 dark:border-slate-700;
-
-	display: block;
-}
-
-.input {
-    @apply border-slate-200;
-    flex-shrink: 1;
-    padding: 10px;
-    width: 100%;
-    height: 40px;
-    font-size: 16px;
-    border: solid 1px;
-    border-radius: 4px 0 0 4px;
-    -webkit-appearance: textfield;
-}
-</style>
