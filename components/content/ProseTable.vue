f382dbd508ddc47b2299d244cc80bc6aca719c43
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-09-15 15:05:41 +0900

    enhance(docs): 横スクロールできる箇所を明示するように

--- components/content/ProseTable.vue
+++ components/content/ProseTable.vue
@@ -1,7 +1,33 @@
 <template>
-	<div class="w-full overflow-x-auto whitespace-nowrap">
-		<table>
+	<div ref="tableContainer" class="w-full overflow-x-auto !overflow-y-hidden whitespace-nowrap mb-4">
+		<table class="!max-w-none !mb-0 !overflow-visible">
 			<slot />
 		</table>
 	</div>
 </template>
+
+<script setup lang="ts">
+import 'scroll-hint/css/scroll-hint.css';
+import { GNodeList } from '@/assets/js/misc/g-node-list.js';
+import ScrollHint from 'scroll-hint';
+
+const { t } = useI18n();
+
+const tableContainer = useTemplateRef('tableContainer');
+
+onMounted(() => {
+	if (!import.meta.client || !tableContainer.value) return;
+
+	new ScrollHint(new GNodeList([tableContainer.value]), {
+		i18n: {
+			scrollable: t('scrollable'),
+		},
+	});
+});
+</script>
+
+<style lang="css">
+.scroll-hint-icon {
+	height: 85px;
+}
+</style>
