d4173d497b849582793649f1410477b553860e78
syuilo <4439005+syuilo@users.noreply.github.com>
2025-04-26 19:47:01 +0900

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@
         </div>
         <main class="relative container mx-auto max-w-screen-xl px-6 mt-32 mb-24 space-y-16">
             <IndexKeyFeatures id="learnMore" />
+						<IndexDecenterized />
             <IndexDonation />
             <GDots class="w-[95%] mx-auto text-accent-600" :space="30" />
             <IndexFeatures />
